e737ee6d830b523b4f8f6ad6fd1a242c610ec89a
bbangjun <lucid9dawn@gmail.com>
2024-02-27 14:08:45 +0900

    feat(boards.controller.js): 컨트롤러 코드에도 타입 사용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
+    // 타입 정의는 선택 사항이지만 타입을 정의해줌으로써 원하는 타입과 다른 코드를 사용할 시 에러가 발생함.
+    // 또한 코드를 읽는 입장에서 더 코드를 쉽게 이해할 수 있음.
     @Get('/')
-    getAllBoard(){
+    getAllBoard(): Board[]{
         return this.boardsService.getAllBoards();
     }
 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards(){
+    // 아래 코드에서 Board[]는 return 되는 타입
+    getAllBoards(): Board[]{
         return this.boards;
     }
 }
