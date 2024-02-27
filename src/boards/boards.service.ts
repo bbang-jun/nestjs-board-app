4f860bdd3eda534c370d67e80465c8adb3adf01d
bbangjun <lucid9dawn@gmail.com>
2024-02-27 14:19:02 +0900

    feat(boards.service.ts): board를 생성하는 createBoard() 메서드 생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +9,17 @@ export class BoardsService {
     // 아래 코드에서 Board[]는 return 되는 타입
     getAllBoards(): Board[]{
         return this.boards;
+    }
+
+    createBoard(title: string, description: string){
+        const board: Board = {
+            id: uuid(), // uuid 모듈을 사용하여 고유한 id 생성
+            title, // title: title과 동일함
+            description, // description: description과 동일함
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
